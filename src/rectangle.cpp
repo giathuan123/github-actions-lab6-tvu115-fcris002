afd9e1606ee0907e13d49dd96ba655716c5daf2d
frxncrxs <fcris002@ucr.edu>
2021-02-18 08:15:23 -0800

    Modified rectangle.cpp file

---
+++ src/rectangle.cpp
@@ -0,0 +1,33 @@
+#include <rectangle.hpp>
+#include <cstdlib>
+
+using namespace std;
+
+Rectangle::Rectangle(): width(0), height(0) {}
+Rectangle::Rectangle(int w, int h){
+  // if (w < 0) width = -w;
+  int  width = abs(w);
+  // else width = w;
+  // if (h < 0) height = h;
+  // if (h < 0) height = -h;
+  int height = abs(h);
+}
+
+void Rectangle::set_width(int w)
+{
+  width = w;
+}
+void Rectangle::set_height(int h)
+{
+  height = h;
+}
+
+int Rectangle::area()
+{
+  return width*height;
+}
+
+int Rectangle::perimeter()
+{
+  return 2*width + 2*height;
+}
