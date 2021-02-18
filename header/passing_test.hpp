4ff44102ec4c0cc5a3f7ef0ba9412bf1354afce8
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-02-18 00:48:19 -0800

    Added Perimeter Test

--- header/passing_test.hpp
+++ header/passing_test.hpp
@@ -21,13 +21,13 @@ TEST(CONSTRUCTOR_TEST, negativeRectTest)
     EXPECT_EQ(r.get_width(), 10);
     EXPECT_EQ(r.get_height(), 5);
 }
-
+// AREA TEST //
 TEST(CONSTRUCTOR_TEST, negativeWidthTest)
 {
     Rectangle r = Rectangle(-10, 5);
     EXPECT_EQ(r.area(), 50);
 }
-// AREA TEST //
+
 TEST(AREA_TEST, negativeHeightTest)
 {
     Rectangle r = Rectangle(-10, 5);
@@ -39,4 +39,23 @@ TEST(AREA_TEST, negativeRectTest)
     Rectangle r = Rectangle(-10, -5);
     EXPECT_EQ(r.area(), 50);
 }
+// PERIMETER TEST //
+TEST(PERIMETER_TEST, negativeWidthTest)
+{
+    Rectangle r = Rectangle(-10, 5);
+    EXPECT_EQ(r.perimeter(), 30);
+}
+
+TEST(PERIMETER_TEST, negativeHeightTest)
+{
+    Rectangle r = Rectangle(-10, 5);
+    EXPECT_EQ(r.perimeter(), 30);
+}
+
+TEST(PERIMETER_TEST, negativeRectTest)
+{
+    Rectangle r = Rectangle(-10, -5);
+    EXPECT_EQ(r.perimeter(), 50);
+}
+
 #endif
