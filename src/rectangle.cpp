e87f9ea8bc3caac4c6ba94e4f8e0ae080dc26157
frxncrxs <fcris002@ucr.edu>
2021-02-18 08:19:19 -0800

    Modified source file for rectangle class

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 Rectangle::Rectangle(): width(0), height(0) {}
 Rectangle::Rectangle(int w, int h){
-  // if (w < 0) width = -w;
-  int  width = abs(w);
+   if (w < 0) width = -w;
+   // int  width = -w;
   // else width = w;
   // if (h < 0) height = h;
-  // if (h < 0) height = -h;
-  int height = abs(h);
+   if (h < 0) height = -h;
+   // int height = -h;
 }
 
 void Rectangle::set_width(int w)
